4c02eb0f391dac918608fd0a38520703878a5120
ricksonthompsonrubcube <rickson.thompson@rubcube.com>
2024-08-02 01:11:33 -0400

    Initial Commit

---
+++ Rotinas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rotinas
+//
+//  Created by Rickson Thompson on 02/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rotinas/RotinasApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotinasApp.swift
+//  Rotinas
+//
+//  Created by Rickson Thompson on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RotinasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
